6f2cc6e6fcab5374024569795c7e861c63b095f9
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-12-28 20:30:38 +0530

    implemented results list functions

--- src/vrex_util.c
+++ src/vrex_util.c
@@ -1,8 +1,8 @@
 /*
-* v-rex: vrex_util.c
-* Created on: 27-Dec-2018
-*
-*/
+ * v-rex: vrex_util.c
+ * Created on: 27-Dec-2018
+ *
+ */
 
 #include <Xbae/Matrix.h>
 #include "vrex_util.h"
@@ -12,3 +12,29 @@ void add_column(Widget mw, char* name, int num, int width) {
 	XbaeMatrixSetColumnWidth(mw, num, width);
 }
 
+vrex_err_t init_results_list(vrex_context* vrex) {
+	if (vrex != NULL) {
+		vrex->results = array_list_new((void (*)(void *))&free_docker_result);
+		return VREX_SUCCESS;
+	}
+	return VREX_E_UNKNOWN;
+}
+
+vrex_err_t results_list_add(vrex_context* vrex, docker_result* res) {
+	if(vrex != NULL && vrex->results != NULL) {
+		array_list_add(vrex->results, res);
+		return VREX_SUCCESS;
+	}
+	return VREX_E_UNKNOWN;
+}
+
+int results_list_length(vrex_context* vrex) {
+	if(vrex != NULL && vrex->results != NULL) {
+		return array_list_length(vrex->results);
+	}
+	return 0;
+}
+
+docker_result* results_list_get_idx(vrex_context* vrex, int i) {
+	return (docker_result*) array_list_get_idx(vrex->results, i);
+}
