d706d872d5cb7e973d603a0279be4964c419df5d
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-11-11 12:44:29 +0530

    added utils, renamed files

---
+++ v-rex-wx/src/vrex_util.c
@@ -0,0 +1,68 @@
+/*
+ * v-rex: vrex_util.c
+ * Created on: 27-Dec-2018
+ *
+ */
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vrex_util.h"
+#include <arraylist.h>
+
+#include <docker_log.h>
+
+vrex_err_t init_results_list(vrex_context* vrex) {
+	if (vrex != NULL) {
+		arraylist_new(&vrex->results, (void (*)(void *)) &free_docker_result);
+		return VREX_SUCCESS;
+	}
+	return VREX_E_UNKNOWN;
+}
+
+vrex_err_t results_list_add(vrex_context* vrex, docker_result* res) {
+	if (vrex != NULL && vrex->results != NULL) {
+		arraylist_add(vrex->results, res);
+		return VREX_SUCCESS;
+	}
+	return VREX_E_UNKNOWN;
+}
+
+int results_list_length(vrex_context* vrex) {
+	if (vrex != NULL && vrex->results != NULL) {
+		return arraylist_length(vrex->results);
+	}
+	return 0;
+}
+
+docker_result* results_list_get_idx(vrex_context* vrex, int i) {
+	return (docker_result*) arraylist_get(vrex->results, i);
+}
+
+// see https://stackoverflow.com/questions/3898840/converting-a-number-of-bytes-into-a-file-size-in-c
+#define DIM(x) (sizeof(x)/sizeof(*(x)))
+
+static const char     *sizes[]   = { "EiB", "PiB", "TiB", "GiB", "MiB", "KiB", "B" };
+static const uint64_t  exbibytes = 1024ULL * 1024ULL * 1024ULL *
+                                   1024ULL * 1024ULL * 1024ULL;
+
+char* calculate_size_str(uint64_t size)
+{
+    char     *result = (char *) malloc(sizeof(char) * 20);
+    uint64_t  multiplier = exbibytes;
+    int i;
+
+    for (i = 0; i < DIM(sizes); i++, multiplier /= 1024)
+    {
+        if (size < multiplier)
+            continue;
+        if (size % multiplier == 0)
+            sprintf(result, "%" PRIu64 " %s", size / multiplier, sizes[i]);
+        else
+            sprintf(result, "%.1f %s", (float) size / multiplier, sizes[i]);
+        return result;
+    }
+    strcpy(result, "0");
+    return result;
+}
---
+++ v-rex-wx/src/vrex_util.h
@@ -0,0 +1,54 @@
+/*
+ * v-rex: vrex_util.h
+ * Created on: 27-Dec-2018
+ *
+ * V-Rex
+ * Copyright (C) 2018 Abhishek Mishra <abhishekmishra3@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_VREX_UTIL_H_
+#define SRC_VREX_UTIL_H_
+
+#include "docker_result.h"
+#include "docker_connection_util.h"
+#include <arraylist.h>
+
+typedef enum {
+	VREX_SUCCESS = 0, VREX_E_UNKNOWN = 1
+} vrex_err_t;
+
+typedef struct vrex_context_t {
+	docker_context* d_ctx;
+	void (*handle_error)(struct vrex_context_t* vrex, docker_result* res);
+	arraylist* results;
+} vrex_context;
+
+vrex_err_t init_results_list(vrex_context* vrex);
+vrex_err_t results_list_add(vrex_context* vrex, docker_result* res);
+int results_list_length(vrex_context* vrex);
+docker_result* results_list_get_idx(vrex_context* vrex, int i);
+
+/**
+ * Get the size in bytes, kb, mb, gb, tb etc.
+ * (higest possible unit)
+ *
+ * \param size
+ * \return string with the description of the size
+ */
+char* calculate_size_str(uint64_t size);
+
+#endif /* SRC_VREX_UTIL_H_ */
