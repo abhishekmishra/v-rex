55b12cc23355b4daf3891488f592dd0d6190f5ca
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-11-11 15:30:37 +0530

    added rich text control lib

---
+++ v-rex-wx/src/DashboardWindow.cpp
@@ -0,0 +1,5 @@
+#include <DashboardWindow.h>
+
+DashboardWindow::DashboardWindow() {
+    
+}
---
+++ v-rex-wx/src/DashboardWindow.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <wx/richtext/richtextctrl.h>
+
+class DashboardWindow: public wxRichTextCtrl {
+public:
+    DashboardWindow();
+};
