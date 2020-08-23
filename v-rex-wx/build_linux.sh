a4bcf8cb7039b21e2f2a42c813d5d620ee152ef5
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-08-23 18:33:26 +0530

    removed hardcoded vcpkg path

--- v-rex-wx/build_linux.sh
+++ v-rex-wx/build_linux.sh
@@ -5,7 +5,7 @@ mkdir -p ./build
 cd ./build
 
 # generate the build files
-cmake .. -DCMAKE_TOOLCHAIN_FILE=/home/abhishek/code/vcpkg/scripts/buildsystems/vcpkg.cmake  -DENABLE_TESTS=On -DENABLE_LUA=On -DwxWidgets_CONFIG_EXECUTABLE=/usr/bin/wx-config
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${VCPKG_HOME}/scripts/buildsystems/vcpkg.cmake  -DENABLE_TESTS=On -DENABLE_LUA=On -DwxWidgets_CONFIG_EXECUTABLE=/usr/bin/wx-config
 # cmake ..
 
 make clean all
